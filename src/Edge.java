8bd01b99689a71fefeaf53122feb8f0a0d9f213f
Nicholas Ho <nicholas_hodj@outlook.com>
2014-10-19 16:14:22 +1100

    Added helper functions to Edge, added list of circuits. Not complete.

--- src/Edge.java
+++ src/Edge.java
@@ -1,15 +1,19 @@
+import java.util.ArrayList;
+
 
 public class Edge {
 	private Node from;
 	private Node to;
 	private int propagationDelay;
 	private int numSimulCircuits;
+	private ArrayList<VirtualCircuit> circuits;
 	
 	public Edge(Node from,Node to, int propDelay, int numSimulCircuits) {
 		this.from = from;
 		this.to = to;
 		propagationDelay = propDelay;
 		this.numSimulCircuits = numSimulCircuits;
+		circuits = new ArrayList<VirtualCircuit>();
 	}	
 	
 	public int getPropagationDelay() {
@@ -26,5 +30,33 @@ public class Edge {
 	
 	public Node getTo(){
 		return to;
+	}
+	
+	public void addCircuit(VirtualCircuit circuit) {
+		circuits.add(circuit);
+	}
+	
+	public ArrayList<VirtualCircuit> getCircuits() {
+		return circuits;
+	}
+	
+	public int numCircuits() {
+		return circuits.size();
+	}
+	
+	public boolean hasCapacity() {
+		if (numCircuits() < numSimulCircuits) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void cleanup(VirtualCircuit newCircuit) {
+		double newEstablishTime = newCircuit.getEstablishTime();
+		
+		for (VirtualCircuit vc : circuits) {
+			
+		}
 	}
 }
